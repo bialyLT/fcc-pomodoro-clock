d013df474f497474933d4491adf602eb66a18f2c
liam <liambialy2@gmail.com>
2024-01-31 15:05:24 -0300

    added session counter

--- src/components/HandlerLength.js
+++ src/components/HandlerLength.js
@@ -1,15 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+function HandlerLength({ title, time, onDecrement, onIncrement, increment, decrement, showTime }) {
 
-function HandlerLength({ title, amount }) {
+    const timeLength = time;
+
+    const handlerClick = (e) => {
+        if (e.target.id === decrement) {
+            onDecrement();
+        }
+        if (e.target.id === increment) {
+            onIncrement();
+        }
+    }
 
     return (
         <div>
-            <h2 className='col-12'>{title}</h2>
-            <div className='row gap-3 col-6 flex-nowrap'>
-                <button className='btn btn-outline-light btn-sm col-4' ><FontAwesomeIcon icon={faArrowDown} /></button>
-                <p className='col-4 fs-2'>{amount}</p>
-                <button className='btn btn-outline-light btn-sm col-4' ><FontAwesomeIcon icon={faArrowUp} /></button>
+            <h2>{title}</h2>
+            <div className='row gap-3 flex-nowrap'>
+                <button className='btn btn-outline-light btn-sm col-4' onClick={handlerClick} id={decrement}><FontAwesomeIcon icon={faArrowDown} size='xl'/></button>
+                <p className='col-4 fs-2 fw-bold'>{showTime(timeLength)}</p>
+                <button className='btn btn-outline-light btn-sm col-4' onClick={handlerClick} id={increment} ><FontAwesomeIcon icon={faArrowUp} size='xl'/></button>
             </div>
         </div>
 
--- src/components/SessionBreak.js
+++ src/components/SessionBreak.js
@@ -5,7 +5,7 @@ function SessionBreak({ time, title }) {
     return (
         <div>
             <h1>{title}</h1>
-            <p>{time}</p>
+            <p className="fs-5 fw-bold">{time}</p>
         </div>
     );
 }
